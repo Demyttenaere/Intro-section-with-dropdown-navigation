1e171a32dc494802bce398026cf9c27dccb929cb
demy <93206611+Demyttenaere@users.noreply.github.com>
2022-09-09 10:41:37 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,55 @@
+const arrow1 = document.querySelectorAll('.ssFeat img');
+const arrow2 = document.querySelectorAll('.ssCompany img');
+const features = document.querySelector('.sousFeatures');
+const company = document.querySelector('.sousCompany');
+const arrow3 = document.querySelectorAll('.iconMenu img');
+const menu = document.querySelector('.navMenu');
+
+arrow1.forEach(icone =>{ 
+    icone.addEventListener('click', function(){
+        
+        if(this.src.includes('icon-arrow-down')){ 
+            this.src ='icon-arrow-up.svg'; 
+            
+            features.classList.remove('hidden');
+            features.classList.remove('hidden2');
+    } 
+        else if (this.src.includes('icon-arrow-up')){   
+            this.src ='icon-arrow-down.svg';
+            
+            features.classList.add('hidden');
+            features.classList.add('hidden2');
+        }})}) 
+    
+    
+        arrow2.forEach(icone =>{ 
+            icone.addEventListener('click', function(){
+                
+                if(this.src.includes('icon-arrow-down')){ 
+                    this.src ='icon-arrow-up.svg'; 
+                    
+                    company.classList.remove('hidden');
+                    company.classList.remove('hidden2');
+            } 
+                else if (this.src.includes('icon-arrow-up')){   
+                    this.src ='icon-arrow-down.svg';
+                    
+                    company.classList.add('hidden');
+                    company.classList.add('hidden2');
+                }})}) 
+
+                arrow3.forEach(icone =>{ 
+                    icone.addEventListener('click', function(){
+                        
+                        if(this.src.includes('icon-menu.svg')){ 
+                            this.src ='icon-close-menu.svg'; 
+                            
+                            menu.classList.remove('hidden');
+                    } 
+                        else if (this.src.includes('icon-close-menu.svg')){   
+                            this.src ='icon-menu.svg';
+                            
+                            menu.classList.add('hidden');
+                        }})}) 
+
+
